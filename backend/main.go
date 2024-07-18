3b5c2ce6855fb43258ed2e7fa94793c06931b3b5
berkay.daglar <eminberkaydaglar@gmail.com>
2024-07-18 22:57:41 +0300

    simplifying main function

--- backend/main.go
+++ backend/main.go
@@ -1,20 +1,17 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	"peaksel/server"
 	"peaksel/storage"
+	"peaksel/utils"
 )
 
 func main() {
-	rdb := storage.NewRedisClient("localhost:6379")
-	defer rdb.Close()
-
+	storageHandler := storage.NewStorageHandler()
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		server.HandleWebSocket(w, r, rdb)
-
+		server.HandleWebSocket(storageHandler, w, r)
 	})
-	log.Println("Starting server on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	utils.InfoLogger.Println("Starting server on :8080")
+	utils.ErrorLogger.Fatal(http.ListenAndServe(":8080", nil))
 }
