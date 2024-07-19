48ef316f70874207e4bdcb6f65fc952f645e5a39
berkay.daglar <eminberkaydaglar@gmail.com>
2024-07-19 07:02:12 +0300

    create connection hub

---
+++ backend/server/hub.go
@@ -0,0 +1,59 @@
+package server
+
+import (
+	"peaksel/utils"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+type Hub struct {
+	connections map[*websocket.Conn]struct{} // Use map to decrease complexity(O(1)). Use struct{} as the value type to save space.
+	mu          sync.Mutex
+}
+
+var (
+	hubInstance *Hub
+	once        sync.Once
+)
+
+func NewHub() *Hub {
+	return &Hub{
+		connections: make(map[*websocket.Conn]struct{}),
+	}
+}
+
+func GetHubInstance() *Hub {
+	once.Do(func() {
+		hubInstance = NewHub()
+	})
+	return hubInstance
+}
+
+func (h *Hub) AddConnection(conn *websocket.Conn) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	h.connections[conn] = struct{}{}
+}
+
+func (h *Hub) RemoveConnection(conn *websocket.Conn) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	if _, ok := h.connections[conn]; ok {
+		delete(h.connections, conn)
+		conn.Close()
+	}
+}
+
+func (h *Hub) Broadcast(message interface{}) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	for conn := range h.connections {
+		err := conn.WriteJSON(message)
+		if err != nil {
+			utils.ErrorLogger.Println("Error broadcasting message:", err)
+			conn.Close()
+			delete(h.connections, conn)
+		}
+	}
+}
