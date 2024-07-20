3f735dca07eb63b5f14333e29bd4420418c0ddf4
berkay.daglar <eminberkaydaglar@gmail.com>
2024-07-20 07:38:08 +0300

    use socket handler

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,13 +1,37 @@
-import { PixelColor } from "../types";
+import { socket } from "../utils/socket";
+class PeexelAPI {
 
-class PeakselAPI {
-
-
-    getPixels(): PixelColor[][] {
-        return Array(2).fill(Array(2).fill('#ffffff'));
+    getPixels() {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log('Response is not given by server in 10 seconds');
+                reject();
+            }, 10 * 1000);
+            socket.once('allPixels', (args) => {
+                resolve(args);
+            });
+            socket.sendMessage({ eventName: 'getPixels' });
+        })
     }
 
+    setPixelData({ x, y, color, username }: { x: Number, y: Number, color: string, username: string }) {
+        return socket.sendMessage({
+            eventName: 'setPixel',
+            x,
+            y,
+            username,
+            color
+        });
+    }
+
+    sendNewChatMessage(message: string, username: string){
+        return socket.sendMessage({
+            eventName: 'newChatMessage',
+            message,
+            username
+        });
+    }
 }
 
 // singleton
-export const api = new PeakselAPI();
+export const api = new PeexelAPI();
