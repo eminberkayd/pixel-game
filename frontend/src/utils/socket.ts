d814c998d901e195f42d3d144397f30a1f622f2a
berkay.daglar <eminberkaydaglar@gmail.com>
2024-07-20 07:20:00 +0300

    use EventEmitter

--- frontend/src/utils/socket.ts
+++ frontend/src/utils/socket.ts
@@ -1,41 +1,49 @@
-const WS_URL = 'ws://localhost:8080/ws';
-class SocketHandler {
+import EventEmitter from "events";
+interface Message extends Record<string, any> {
+    eventName: string;
+}
+class SocketHandler extends EventEmitter {
     private socket: WebSocket;
-    private subscribers: ((msg: string)=> any)[] = []
 
-    constructor(url: string){
+    constructor(url: string) {
+        super();
         this.socket = new WebSocket(url);
         this.socket.onopen = () => {
             console.log('WebSocket connection established');
         };
-        
+
         this.socket.onmessage = (event) => {
-            const message = JSON.parse(event.data);
-            console.log('Received message:', message);
-            this.subscribers.forEach(cb => {
-                cb(message);
-            })
+            this.handleNewMessage(event.data);
         };
+
         this.socket.onclose = (event) => {
             console.log('WebSocket connection closed:', event);
         };
-    
+
         this.socket.onerror = (error) => {
             console.error('WebSocket error:', error);
         };
     }
 
-    subscribeMessageEvent(cb: (msg: string) => any){
-        this.subscribers.push(cb);
+    handleNewMessage(message: Message) {
+        console.log('Received message: ', message);
+        console.log('typeof message: ', typeof message);
+        const { eventName, ...args } = message;
+        this.emit(eventName, args);
     }
 
-    sendMessage(message: string){
+    sendNewMessage({ eventName, ...args }: Message) {
         if (this.socket && this.socket.readyState === WebSocket.OPEN) {
-            this.socket.send(message);
+            const message = { eventName, ...args };
+            const jsonMessage = JSON.stringify(message);
+            this.socket.send(jsonMessage);
+            return true;
         } else {
             console.error('Message is not sent. WebSocket is not open');
         }
+        return false;
     }
 }
 
-export const socketHandler = new SocketHandler(WS_URL);
+const WS_URL = 'ws://localhost:8080/ws';
+export const socket = new SocketHandler(WS_URL);
