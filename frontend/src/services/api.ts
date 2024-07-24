b9316f19ccb07f77da3440ad80160d43ffdee86d
berkay.daglar <eminberkaydaglar@gmail.com>
2024-07-24 22:30:58 +0300

    add getOnlineUsers functionality

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,14 +1,33 @@
+import { PixelData } from "../types";
 import { socket } from "../utils/socket";
+
+interface PixelValues {
+    [key: string]: PixelData
+}
 class PeexelAPI {
 
-    getPixels() {
+    getPixels(): Promise<PixelData[][]> {
         return new Promise((resolve, reject) => {
-            setTimeout(() => {
+            const timeout = setTimeout(() => {
                 console.log('Response is not given by server in 10 seconds');
-                reject();
+                resolve(Array(10).fill(Array(10).fill({
+                    color: '#122321',
+                    username: 'string',
+                })));
             }, 10 * 1000);
-            socket.once('allPixels', (args) => {
-                resolve(args);
+            socket.once('allPixels', ({ values }: { values: PixelValues }) => {
+                clearTimeout(timeout);
+                let myArray = Array(10).fill(Array(10).fill({
+                    color: '#122321',
+                    username: 'string',
+                }));
+                for (const key in values) {
+                    let [x, y] = key.split(':').map(Number);
+                    if(x <= 10 && y <= 10){
+                        myArray[x][y] = values[key];
+                    }
+                }
+                resolve(myArray);
             });
             socket.sendMessage({ eventName: 'getPixels' });
         })
@@ -24,12 +43,32 @@ class PeexelAPI {
         });
     }
 
-    sendNewChatMessage(message: string, username: string){
+    sendNewChatMessage(message: string) {
         return socket.sendMessage({
             eventName: 'newChatMessage',
-            message,
-            username
+            message
         });
+    }
+
+    sendJoinEvent(username: string) {
+        return socket.sendMessage({
+            eventName: 'join',
+            username
+        })
+    }
+
+    getOnlineUsers() {
+        return new Promise((resolve, reject) => {
+            const timeout = setTimeout(() => {
+                console.log('Response is not given by server in 10 seconds');
+                resolve([]);
+            }, 10 * 1000);
+            socket.once('onlineUsersList', (payload) => {
+                clearTimeout(timeout);
+                resolve(payload.usernames as string[]);
+            });
+            socket.sendMessage({ eventName: 'getOnlineUsers' });
+        })
     }
 }
 
