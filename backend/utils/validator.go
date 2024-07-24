6cbd2c41be31805729041983d054f4a1a0357e07
berkay.daglar <berkay.daglar@v-count.com>
2024-07-24 07:10:23 +0300

    use mapstructure

--- backend/utils/validator.go
+++ backend/utils/validator.go
@@ -1,27 +1,37 @@
 package utils
 
-import "peexel/models"
+import (
+	"peexel/models"
 
-func ValidateSetPixelMessage(message interface{}) (models.SetPixelMessage, bool) {
-	setPixelMessage, validated := message.(models.SetPixelMessage)
-	if !validated {
+	"github.com/mitchellh/mapstructure"
+)
+
+func ValidateSetPixelMessage(message map[string]interface{}) (models.SetPixelMessage, bool) {
+	var setPixelMessage models.SetPixelMessage
+	err := mapstructure.Decode(message, &setPixelMessage)
+	if err != nil {
 		ErrorLogger.Println("Invalid message format for set pixel message")
+		return models.SetPixelMessage{}, false
 	}
-	return setPixelMessage, validated
+	return setPixelMessage, true
 }
 
-func ValidateJoinMessage(message interface{}) (models.JoinMessage, bool) {
-	joinMessage, validated := message.(models.JoinMessage)
-	if !validated {
+func ValidateJoinMessage(message map[string]interface{}) (models.JoinMessage, bool) {
+	var joinMessage models.JoinMessage
+	err := mapstructure.Decode(message, &joinMessage)
+	if err != nil {
 		ErrorLogger.Println("Invalid message format for join message")
+		return models.JoinMessage{}, false
 	}
-	return joinMessage, validated
+	return joinMessage, true
 }
 
-func ValidateNewChatMessage(message interface{}) (models.NewChatMessage, bool) {
-	newChatMessage, validated := message.(models.NewChatMessage)
-	if !validated {
+func ValidateNewChatMessage(message map[string]interface{}) (models.NewChatMessage, bool) {
+	var newChatMessage models.NewChatMessage
+	err := mapstructure.Decode(message, &newChatMessage)
+	if err != nil {
 		ErrorLogger.Println("Invalid message format for new chat message")
+		return models.NewChatMessage{}, false
 	}
-	return newChatMessage, validated
+	return newChatMessage, true
 }
