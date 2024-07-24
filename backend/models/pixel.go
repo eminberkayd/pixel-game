50634aa397e005c14c1b79179d0438d2b3e3d2d3
berkay.daglar <berkay.daglar@v-count.com>
2024-07-24 06:35:57 +0300

    add message formats

--- backend/models/pixel.go
+++ backend/models/pixel.go
@@ -5,10 +5,21 @@ import (
 )
 
 type SetPixelMessage struct {
-	X        int    `json:"x"`
-	Y        int    `json:"y"`
-	Color    string `json:"color"`
-	Username string `json:"username"`
+	EventName string `json:"eventName"`
+	X         int    `json:"x"`
+	Y         int    `json:"y"`
+	Color     string `json:"color"`
+	Username  string `json:"username"`
+}
+
+type JoinMessage struct {
+	EventName string `json:"eventName"`
+	Username  string `json:"username"`
+}
+
+type NewChatMessage struct {
+	EventName string `json:"eventName"`
+	Message   string `json:"message"`
 }
 
 // PixelData represents the data stored for each pixel.
